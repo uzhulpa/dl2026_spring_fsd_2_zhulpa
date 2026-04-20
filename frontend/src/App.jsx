50241744f1a6e77b97ec7abbc2cbfa79aef98768
ulyana <uzh0976@gmail.com>
2026-04-21 02:55:19 +0300

    feat(frontend): initialize app with routing and basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
+import AppRoutes from './routes';
+
 function App() {
-  return (
-    <>
-      <h1>Hello world!</h1>
-    </>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
