bde633878b36868225f172b027393cdfe6db3264
ulyana <uzh0976@gmail.com>
2026-04-21 07:19:46 +0300

    feat(backend): add crud api with questions for admin

---
+++ backend/src/routes/admin/AdminQuestionsRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import validate from "../../middlewares/validate.js";
+import adminQuestionSchema from "../../validations/questionValidation.js";
+
+import AdminQuestionsController from "../../controllers/admin/AdminQuestionsController.js";
+
+const AdminQuestionsRouter = new Router();
+
+AdminQuestionsRouter.get('/', AdminQuestionsController.getQuestions);
+AdminQuestionsRouter.get('/:questionId', AdminQuestionsController.getQuestionById);
+AdminQuestionsRouter.put('/:questionId', validate(adminQuestionSchema), AdminQuestionsController.updateQuestionById);
+AdminQuestionsRouter.post('/', validate(adminQuestionSchema), AdminQuestionsController.addQuestion);
+
+export default AdminQuestionsRouter;
