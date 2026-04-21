5d473c6284c548053709129113f39b8756e5ba75
ulyana <uzh0976@gmail.com>
2026-04-21 14:13:07 +0300

    fix(backend): add questions_count attribute to admin get collections response

--- backend/src/services/admin/AdminCollectionsService.js
+++ backend/src/services/admin/AdminCollectionsService.js
@@ -1,14 +1,40 @@
 import { Collection, Question, CollectionQuestion, sequelize } from "../../models/index.js";
+import { fn, col } from "sequelize";
 
 class AdminCollectionsService {
     async getCollections(limit=10, offset=0) {
-        const result = await Collection.findAll({
+        const collections = await Collection.findAll({
+            attributes: [
+                "id",
+                "name",
+                "description",
+                "random_order",
+                "author_id",
+                "created_at",
+                [sequelize.cast(fn("COUNT", col("CollectionQuestions.id")), "integer"), "questions_count"],
+            ],
+            include: [
+                {
+                    model: CollectionQuestion,
+                    attributes: [],
+                    required: false,
+                },
+            ],
+            group: [
+                "Collection.id",
+                "Collection.name",
+                "Collection.description",
+                "Collection.random_order",
+                "Collection.author_id",
+                "Collection.created_at",
+            ],
+            subQuery: false,
             limit: limit,
             offset: offset,
             order: [['created_at', 'ASC']]
         });
-
-        return result;
+        
+        return collections;
     }
 
     async getCollectionById(collectionId) {
